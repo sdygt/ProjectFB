30b9f9469934d8439e9979c16b48bffa1ace62ab
sdygt <sdygt@users.noreply.github.com>
2015-06-10 01:26:24 +0800

    隐藏URL中admin.php并修复.htaccess

--- ProjectFB/config.php
+++ ProjectFB/config.php
@@ -5,5 +5,7 @@ return array(
 	'TOKEN_NAME'=>'__hash__',    // 令牌验证的表单隐藏字段名称
 	'TOKEN_TYPE'=>'md5',  //令牌哈希验证规则 默认为MD5
 	'TOKEN_RESET'=>true,  //令牌验证出错后是否重置令牌 默认为true
+	'URL_MODEL'=> 2// URL访问模式,可选参数0、1、2、3,代表以下四种模式：
+    // 0 (普通模式); 1 (PATHINFO 模式); 2 (REWRITE  模式); 3 (兼容模式)  默认为PATHINFO 模式，提供最好的用户体验和SEO支持
 );
 ?>
